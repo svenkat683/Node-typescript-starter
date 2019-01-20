c3696582aeebd657f58b22a4e0aaebc858eb8581
venkat <venkat@avantari.org>
2019-01-21 02:22:26 +0530

    Added fully functional lambda setup

--- src/app/config/dbConnection.ts
+++ src/app/config/dbConnection.ts
@@ -2,6 +2,7 @@ import mongo from "mongoose";
 import { MONGODB_URL } from "../utils/secrets";
 
 const dbUrl = MONGODB_URL;
+console.log("dbUrl: ", dbUrl);
 function mongoConnect () {
     mongo.connect(dbUrl, { useNewUrlParser: true } ).then(() => {
         console.log("database connected successfully.");
